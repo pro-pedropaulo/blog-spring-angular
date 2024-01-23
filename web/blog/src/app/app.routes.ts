96803b4917a392237c1fbecb4735bafb54860136
Pedro Silva <pedro.silva@b2ml.com.br>
2024-01-23 11:46:45 -0300

    atualizada rotas

--- web/blog/src/app/app.routes.ts
+++ web/blog/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './../components/home/home.component';
 import { LoginComponent } from './../components/login/login.component';
 import { RegisterComponent } from '../components/register/register.component';
+import { AddPostComponent } from '../components/add-post/add-post.component';
 
 export const routes: Routes = [
     {'path': '', component: HomeComponent },
     {'path': 'login', component: LoginComponent },
-    {'path': 'register', component: RegisterComponent }
+    {'path': 'register', component: RegisterComponent },
+    {'path': 'add-post', component: AddPostComponent}
 
 ];
