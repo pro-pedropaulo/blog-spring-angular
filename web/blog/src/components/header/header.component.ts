4a6c150c54a4c035f8ee1e937fe7b51e0388f30f
Pedro Silva <pedro.silva@b2ml.com.br>
2024-01-23 11:46:17 -0300

    login e logout sendo realizado

--- web/blog/src/components/header/header.component.ts
+++ web/blog/src/components/header/header.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -12,8 +13,16 @@ export class HeaderComponent {
 
   constructor(private router: Router) { }
 
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
 
   navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
     this.router.navigate(['/login']);
   }
 
--- web/blog/src/components/home/home.component.ts
+++ web/blog/src/components/home/home.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  constructor(private router: Router) { }
+
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  navigateToAddPost() {
+    this.router.navigate(['/add-post']);
+  }
 }
